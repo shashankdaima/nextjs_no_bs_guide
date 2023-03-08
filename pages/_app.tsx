1962cef0c58bcfa43d14e14075c6c97e8bed4791
shashank <daimashashank10@gmail.com>
2023-03-08 05:34:33 +0530

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
+import styles from "@/styles/App.module.css";
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import { Container } from 'react-bootstrap';
+import Head from 'next/head';
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Head>
+        <title>NextJS News App</title>
+        <meta name="description" content="NextJS crash course by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
+    </div>
+
+  );
 }
---
+++ pages/search.tsx
@@ -0,0 +1,5 @@
+const SearchPage = () => {
+    return ( <><p>HELLOWORLD</p></> );
+}
+ 
+export default SearchPage;
